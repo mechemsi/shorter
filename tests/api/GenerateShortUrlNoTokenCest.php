f6e1371f1e5c78cf890bfd30a5501f68db2c4cf1
Dominykas <dominykas.vilipas@gmail.com>
2023-01-11 13:48:32 +0200

    update for prod

--- tests/api/GenerateShortUrlNoTokenCest.php
+++ tests/api/GenerateShortUrlNoTokenCest.php
@@ -12,15 +12,14 @@ namespace App\Tests\Api;
 
 use App\Tests\ApiTester;
 
-class GenerateShortUrlCestNoToken
+class GenerateShortUrlNoTokenCest
 {
     public function generateShortUrlNoTokenError(ApiTester $tester): void
     {
-        $tester->sendPostAsJson('/generate', [
+        $tester->sendPost('/generate', [
             'longUrl' => 'https://somthing.com',
         ]);
         $tester->seeResponseCodeIs(401);
-        $tester->seeResponseIsJson();
     }
 
     public function generateShortUrlBadTokenError(ApiTester $tester): void
